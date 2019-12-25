910c49ad87d6f11d78249c3b9ae9dfe45d926442
Sean Engelhart <seansre@gmail.com>
2019-12-25 13:07:50 -0600

    fixed indentation

--- gsgc.c
+++ gsgc.c
@@ -12,22 +12,22 @@ void printWelcomeMessage()
 
 void draw(char **out, int rows, int cols)
 {
-  for(int i = 0; i < rows; i++) {
-    for(int j = 0; j < cols; j++) {
-        // TODO: fill in w/ if statement
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            // TODO: fill in w/ if statement
+        }
     }
-  }
 }
 
 int main(void) 
 {
-// TODO: possibly hardcode welcome message in?
-//   char[6][43] welcomeMessage = {
-//       {  ______     ______     ______     ______   },
-//       { /\  ___\   /\  ___\   /\  ___\   /\  ___\  },
-//       { \ \ \__ \  \ \___  \  \ \ \__ \  \ \ \____ },
-//       {  \ \_____\  \/\_____\  \ \_____\  \ \_____\},
-//       {   \/_____/   \/_____/   \/_____/   \/_____/}
-//   };
+    // TODO: possibly hardcode welcome message in?
+    //   char[6][43] welcomeMessage = {
+    //       {  ______     ______     ______     ______   },
+    //       { /\  ___\   /\  ___\   /\  ___\   /\  ___\  },
+    //       { \ \ \__ \  \ \___  \  \ \ \__ \  \ \ \____ },
+    //       {  \ \_____\  \/\_____\  \ \_____\  \ \_____\},
+    //       {   \/_____/   \/_____/   \/_____/   \/_____/}
+    //   };
     printWelcomeMessage();
 }
