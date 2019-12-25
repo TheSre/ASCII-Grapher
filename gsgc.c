3c5919efbe85c5ae49e6e015c1f38663ca75e897
gar-source <gar@GarrettPC.localdomain>
2019-12-25 16:55:21 -0600

    spacing fix

--- gsgc.c
+++ gsgc.c
@@ -15,7 +15,7 @@ void draw(char **out, int rows, int cols)
 {
     for(int i = 0; i <= rows; i++) {
         for(int j = 0; j <= cols; j++) {
-			if(j == cols) {
+			if( j == cols) {
 				printf("\n");
 			} else {
 				if(out[i][j] == "#") {
