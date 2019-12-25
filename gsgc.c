9bb69f6399277be55336813b6e1a4c7836fb1f3a
Sean Engelhart <seansre@gmail.com>
2019-12-25 13:01:46 -0600

    added ASCII art welcome screen--we should talk about alternative methods for this

--- gsgc.c
+++ gsgc.c
@@ -1,12 +1,33 @@
-#import <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void draw(char out[][], int rows, int cols)
+void printWelcomeMessage()
+{
+    // Check if ASCII.txt has been renamed or removed
+    if (system("cat ASCII.txt")) {
+        system("clear");
+        printf("Welcome to the GSGC!\nPlease enter a function to graph: ");
+    }
+}
+
+void draw(char **out, int rows, int cols)
 {
   for(int i = 0; i < rows; i++) {
     for(int j = 0; j < cols; j++) {
-      if(i % 5 && ) {
-
-      }
+        // TODO: fill in w/ if statement
     }
   }
+}
+
+int main(void) 
+{
+// TODO: possibly hardcode welcome message in?
+//   char[6][43] welcomeMessage = {
+//       {  ______     ______     ______     ______   },
+//       { /\  ___\   /\  ___\   /\  ___\   /\  ___\  },
+//       { \ \ \__ \  \ \___  \  \ \ \__ \  \ \ \____ },
+//       {  \ \_____\  \/\_____\  \ \_____\  \ \_____\},
+//       {   \/_____/   \/_____/   \/_____/   \/_____/}
+//   };
+    printWelcomeMessage();
 }
