e5d10dfa5575679942200f8af402b24515545e9b
gar-source <gar@GarrettPC.localdomain>
2019-12-25 16:45:57 -0600

    Updated draw function, decided to move axis # printing to matrix creation

--- gsgc.c
+++ gsgc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+void printWelcomeMessage();
+void draw(char **out, int rows, int cols);
 void printWelcomeMessage()
 {
     // Check if ASCII.txt has been renamed or removed
@@ -12,9 +13,18 @@ void printWelcomeMessage()
 
 void draw(char **out, int rows, int cols)
 {
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
-            // TODO: fill in w/ if statement
+    for(int i = 0; i <= rows; i++) {
+        for(int j = 0; j <= cols; j++) {
+			if(j == cols) {
+				printf("\n");
+			} else {
+				if(out[i][j] == "#") {
+					printf("\033[0;31m");
+				} else {
+					printf("\033[0m");
+				}
+				printf("%c",out[i][j]);
+			}
         }
     }
 }
