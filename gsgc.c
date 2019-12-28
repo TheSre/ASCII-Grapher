c1b6913fa034bb8ccf6393aa0a706e4e9a61a9c7
Sean Engelhart <seansre@gmail.com>
2019-12-28 14:38:41 -0600

    added function struct

--- gsgc.c
+++ gsgc.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#define DEFAULT_FUNCTION_BUFF_SIZE 50
+
 void printWelcomeMessage();
 void draw(char **out, int rows, int cols);
+
+typedef struct {
+    char* buf;
+    int length;
+}Function;
+
 void printWelcomeMessage()
 {
     // Check if ASCII.txt has been renamed or removed
     if (system("cat .ASCII.txt")) {
         system("clear");
-        printf("Welcome to the GSGC!\nPlease enter a function to graph: ");
+        printf("Welcome to the GSGC!\n");
     }
+}
+
+void getInput(Function* function, int *rows, int *cols) 
+{
+    printf("Please enter: \n");
+    printf("\ta function to graph: ");
+    scanf("%s", function->buf);
+    printf("\twindow size (vertical): ");
+    scanf("%d", rows);
+    printf("\twindow size (horizontal): ");
+    scanf("%d", cols);
+
+    // TODO: delete below (just for testing)
+    printf("%s, %d, %d\n", function->buf, *rows, *cols);
 }
 
 void draw(char **out, int rows, int cols)
@@ -41,4 +64,8 @@ int main(void)
     //       {   \/_____/   \/_____/   \/_____/   \/_____/}
     //   };
     printWelcomeMessage();
+    Function*  input= malloc(DEFAULT_FUNCTION_BUFF_SIZE);
+    int *rows = malloc(sizeof(int));
+    int *cols = malloc(sizeof(int));
+    getInput(input, rows, cols);
 }
