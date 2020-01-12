2f25f51e4d760b4779ed20749bb128f714478e62
SRE <seansre@gmail.com>
2020-01-12 14:29:33 -0600

    fixed splitFunctionStorage naming issue

--- gsgc.c
+++ gsgc.c
@@ -257,7 +257,7 @@ int main(void)
 
     getInput(&function, &rows, &cols);
     adjustSize(&rows, &cols);
-    char** splitFunctionResult = splitFunction(&function);
+    char** splitFunctionStorage = splitFunction(&function);
     // TODO: be sure to free up entire tree
     TreeNode* root = buildFunctionTree(splitFunctionStorage, 0);
     printf("got thru building tree\n");// TODO: delete
